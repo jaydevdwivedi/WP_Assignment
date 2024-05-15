69c7826642b79b55110b02e766ea11c2ecab684c
Jaydev Dwivedi <jaydevdwivedi2022@gmail.com>
2024-05-15 18:34:05 +0530

    update

--- Assignment-7/q3_index.js
+++ Assignment-7/q3_index.js
@@ -1,8 +1,8 @@
 let sentences = ["Hello World!"];
 let word = "World";
-for(let sentence in sentences){
+for (let sentence in sentences) {
     console.log(sentences[sentence]);
-    if(sentences[sentence].indexOf(word)){
+    if (sentences[sentence].indexOf(word)) {
         console.log(sentences[sentence].indexOf(word));
         break;
     }
